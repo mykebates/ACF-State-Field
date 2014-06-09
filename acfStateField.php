604e5c2fd2ed6d1f1ad5664c8424152737719e3f
Dreb Bits <drebbitanghol@gmail.com>
2014-06-09 11:13:35 +0800

    Updated Plugin Info

--- acfStateField.php
+++ acfStateField.php
@@ -1,14 +1,30 @@
 <?php
 
 /*
-Plugin Name: Advanced Custom Fields State
-Plugin URI: http://github.com/drebbits/acf-child_field
-Description: A field type for ACF populates drop down with states.
-Version: 1.0.0
-Author: Dreb Bits & Myke Bates
-Author URI: http://drebbits.com
-License: GPLv2 or later
-License URI: http://www.gnu.org/licenses/gpl-2.0.html
+Plugin Name: ACF State Field
+Plugin URI: https://github.com/mykebates/ACF-State-Field
+Description: Adds a new ACF field option that lists all US states.
+Version: 2.0
+Author: Myke Bates
+Author URI: http://mykebates.com/
+Author Email: contact@mykebates.com
+License:
+
+  Copyright 2013 Myke Bates (contact@mykebates.com)
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License, version 2, as 
+  published by the Free Software Foundation.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+  
 */
 
 
